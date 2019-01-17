53ebe55d35ee44de72b4e20750beb7b4c623c8dc
flische <flische@gmail.com>
2019-01-17 14:03:49 -0800

    setup routing and linksand refactored view components

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,6 +7,7 @@ import { MessageService } from './message.service';
 @Injectable({
   providedIn: 'root'
 })
+
 export class HeroService {
 
   constructor( private messageService: MessageService ) { }
@@ -15,6 +16,12 @@ export class HeroService {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 
 }
