9a1cf3093377fc278410cce7f97f4a3f4058a2da
flische <flische@gmail.com>
2019-01-16 23:11:07 -0800

    setup display list and selected hero details view

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Thor' },
+    { id: 12, name: 'Loki' },
+    { id: 13, name: 'Hulk' },
+    { id: 14, name: 'Black Widow' },
+    { id: 15, name: 'Black Panther' },
+    { id: 16, name: 'Captain America' },
+    { id: 17, name: 'Iron Man' },
+    { id: 18, name: 'Spider Man' },
+    { id: 19, name: 'Dare Devil' },
+    { id: 20, name: 'The Punisher' }
+];
